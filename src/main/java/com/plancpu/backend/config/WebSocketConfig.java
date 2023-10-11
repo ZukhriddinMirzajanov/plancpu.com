8f74e7c8b048599b9dc5d6d34bc736fe510d6d71
zmi <zmi@globeteam.com>
2023-10-11 11:22:25 +0500

    added allowed url to WebSocketConfig

--- src/main/java/com/plancpu/backend/config/WebSocketConfig.java
+++ src/main/java/com/plancpu/backend/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:3000","https://plancpu.com").withSockJS();
     }
 }
